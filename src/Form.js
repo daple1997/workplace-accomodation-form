96c0ef5d7f8fa08fb6f015898c8cbcbb01f12f12
pled2720 <d.pleskach@uleth.ca>
2022-12-26 19:03:36 -0700

    Input fields for ID, Department, Empyment status, and Email are completed

--- src/Form.js
+++ src/Form.js
@@ -3,12 +3,16 @@ import {useState} from 'react';
 
 export default function Form(){
     const [name, setName] = useState("");
+    const [id, setID] = useState("");
+    const [dept, setDept] = useState("");
+    const [es, setES] = useState("");
+    const [email, setEmail] = useState("");
     return (
         <div>
             <form>
-                <label htmlFor="name"> 
-                Name: 
-                <input 
+                <p>
+                    Name: 
+                    <input 
                         type="text" 
                         id="name" 
                         value={name} 
@@ -16,7 +20,48 @@ export default function Form(){
                         onChange={(e)=>setName(e.target.value)}
                         />
                         
-                </label>
+                </p> 
+                <p> 
+                    ID: 
+                    <input 
+                        type="text" 
+                        id="ID" 
+                        value={id} 
+                        placeholder="ID"
+                        onChange={(e)=>setID(e.target.value)}
+                        />
+                        
+                </p>
+                <p> 
+                    Department: 
+                        <input 
+                            type="text" 
+                            id="Department" 
+                            value={dept} 
+                            placeholder="Department"
+                            onChange={(e)=>setDept(e.target.value)}
+                        />
+                    </p>
+                <p> 
+                    Employment Status: 
+                    <input 
+                        type="text" 
+                        id="Employment status" 
+                        value={es} 
+                        placeholder="Employment status"
+                        onChange={(e)=>setES(e.target.value)}
+                    /> 
+                </p>
+                <p> 
+                    Email: 
+                    <input 
+                        type="text" 
+                        id="Email" 
+                        value={email} 
+                        placeholder="Email"
+                        onChange={(e)=>setEmail(e.target.value)}
+                    /> 
+                </p>
             </form>
         </div>
     );
